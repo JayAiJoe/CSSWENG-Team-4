6ab6a5213d76a190beda908881730989db1e6888
GabPua <gabriel_pua@dlsu.edu.ph>
2021-07-27 16:12:34 +0800

    modify: Major changes to Calculator.java to incorporate FeeTables

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -2,12 +2,45 @@ package model;
 
 // TODO: check invalid inputs i.e. negative inputs, maybe
 
+import java.util.ArrayList;
+
 /**
  * This class is responsible for all computations related
  * to the employees' wages. These include the base wages of employees
  * as well as government fees such as SSS, PhilHealth, and Pag-Ibig.
  */
 public class Calculator {
+    /** The instance of the Calculator class. */
+    private static Calculator instance = null;
+    /** The FeeTable for the PhilHealth Fee. */
+    private FeeTable philhealthFeeTable;
+    /** The FeeTable for the Pag-Ibig Fee. */
+    private FeeTable pagibigFeeTable;
+    /** The FeeTable for the SSS Fee. */
+    private FeeTable sssFeeTable;
+
+    /**
+     * A constructor for a Calculator.
+     */
+    private Calculator() {
+        philhealthFeeTable = new FeeTable(FeeTable.PHILHEALTH_FILE_NAME);
+        pagibigFeeTable = new FeeTable(FeeTable.PAG_IBIG_FILE_NAME);
+        sssFeeTable = new FeeTable(FeeTable.SSS_FILE_NAME);
+    }
+
+    /**
+     * Returns the instance of the Calculator class. If no instance
+     * of the Calculator has been instantiated, this method will create
+     * a new Calculator instance.
+     * @return the instance of the Calculator class
+     */
+    public static Calculator getInstance() {
+        if (instance == null) {
+            instance = new Calculator();
+        }
+        return instance;
+    }
+
     /**
      * Computes the total salary of an employee based on the
      * hourly rate of an employee, the number of work days, the
@@ -18,7 +51,7 @@ public class Calculator {
      * @param minutesOT the number of minutes overtime
      * @return the total salary of an employee
      */
-    public static double computeSalary(double rate, int workDays, int minutesLate,
+    public double computeSalary(double rate, int workDays, int minutesLate,
                                        int minutesOT) {
         return rate * 8 * workDays + computeOvertime(rate, minutesOT) -
                 computeLateFee(rate, minutesLate);
@@ -32,7 +65,7 @@ public class Calculator {
      * @param minutesOT the number of minutes overtime
      * @return the Overtime bonus to be added to an employee's salary
      */
-    private static double computeOvertime(double rate, int minutesOT) {
+    private double computeOvertime(double rate, int minutesOT) {
         return rate * minutesOT / 60 * 1.25;
     }
 
@@ -44,58 +77,64 @@ public class Calculator {
      * @param minutesLate the number of minutes late
      * @return the Late Fee to be deducted from an employee's salary
      */
-    private static double computeLateFee(double rate, int minutesLate) {
+    private double computeLateFee(double rate, int minutesLate) {
         return rate * minutesLate / 60;
     }
 
     /**
      * Computes for the PhilHealth Fee to be paid by an employee
      * based on the employee's monthly basic salary.
-     * @param salary is the monthly basic salary of an employee
+     * @param salary the monthly basic salary of an employee
      * @return the PhilHealth Fee to be paid by an employee
+     * @throws Exception if the salary does not fit any range
      */
-    public static double computePhilHealthFee(double salary) {
-        if (salary <= 10000) {
-            return 175;
-        } else if (salary < 70000) {
-            return 0.035 * salary / 2;
-        } else {
-            return 1225;
-        }
+    public double computePhilHealthFee(double salary) throws Exception {
+        return computeGovtFee(salary, philhealthFeeTable.getFormulas());
     }
 
     /**
      * Computes for the Pag-Ibig Fee to be paid by an employee
      * based on the employee's monthly basic salary.
-     * @param salary is the monthly basic salary of on employee
+     * @param salary the monthly basic salary of on employee
      * @return the Pag-Ibig Fee to be paid by an employee
+     * @throws Exception if the salary does not fit any range
      */
-    public static double computePagIbigFee(double salary) {
-        if (salary <= 1500) {
-            return salary * 0.01;
-        } else {
-            return Math.min(salary * 0.02, 100);
-        }
+    public double computePagIbigFee(double salary) throws Exception {
+        return computeGovtFee(salary, pagibigFeeTable.getFormulas());
     }
 
     /**
      * Computes for the SSS Fee to be paid by an employee
      * based on the employee's monthly basic salary.
-     * @param salary is the monthly basic salary of an employee
+     * @param salary the monthly basic salary of an employee
      * @return the SSS Fee to be paid by an employee
+     * @throws Exception if the salary does not fit any range
      */
-    public static double computeSSSFee(double salary) {
-        if (salary < 3250) {
-            return 135;
-        }
+    public double computeSSSFee(double salary) throws Exception {
+        return computeGovtFee(salary, sssFeeTable.getFormulas());
+    }
 
-        double check = 3500;
-        while (check <= 19500) {
-            if (salary >= check - 250 && salary < check + 250) {
-                return check * 0.045;
+    /**
+     * Computes for a government fee to be paid by an employee
+     * based on the employee's monthly basic salary and the
+     * fee table of the government fee.
+     * @param salary the monthly basic salary of an employee
+     * @param formulas the fee table of the government fee
+     * @return the government fee to be paid by an employee
+     * @throws Exception if the salary does not fit any range
+     */
+    private double computeGovtFee(double salary, ArrayList<ArrayList<Double>> formulas)
+            throws Exception {
+        for (ArrayList<Double> row: formulas) {
+            double lower_bound = row.get(0);
+            double upper_bound = row.get(1);
+            double rate = row.get(2);
+            double constant = row.get(3);
+
+            if (salary >= lower_bound && salary < upper_bound) {
+                return rate * salary + constant;
             }
-            check += 500;
         }
-        return 900;
+        throw new Exception("Salary does not fit any range");
     }
 }
