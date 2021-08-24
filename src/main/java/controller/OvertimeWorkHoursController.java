be1adec374b8f893301d741b891de66372a4cb97
GabPua <gabriel_pua@dlsu.edu.ph>
2021-08-24 17:25:32 +0800

    add: implemented display of pending overtime entries in table

--- src/main/java/controller/OvertimeWorkHoursController.java
+++ src/main/java/controller/OvertimeWorkHoursController.java
@@ -1,18 +1,19 @@
 package controller;
 
 import driver.Driver;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import model.PayrollEntry;
+import model.OvertimeEntry;
+import model.OvertimeHandler;
 
-import java.io.IOException;
-import java.util.Optional;
+import java.util.Date;
 
-public class OvertimeWorkHoursController extends Controller{
+public class OvertimeWorkHoursController extends Controller {
     @FXML
     private AnchorPane navBar_container;
 
@@ -20,19 +21,22 @@ public class OvertimeWorkHoursController extends Controller{
      * initialization of Overtime Table related objects
      */
     @FXML
-    private TableColumn nameTc, overtimeTc, dateTc, buttonTc;
-
+    private TableView<OvertimeEntry> overtimeTv;
     @FXML
-    private TableView overtimeTv;
+    private TableColumn<OvertimeEntry, String> nameTc, dateTc;
+    @FXML
+    private TableColumn<OvertimeEntry, Integer> overtimeTc;
+    @FXML
+    private TableColumn<OvertimeEntry, Boolean> buttonTc;
 
     /**
      * initialization of work hours and overtime related objects
      */
     @FXML
-    private Button workEditBtn, workCancelBtn, workSaveBtn, overtimeEditBtn, overtimeCancelBtn, overtimeSaveBtn,
-            acceptBtn, rejectBtn, checkAllBtn;
-    @FXML
-    private TextField overtimeStartTf, overtimeEndTf, workStartTf, workEndTf, workStartTf2, workEndTf2;
+    private Button acceptBtn, rejectBtn, checkAllBtn;
+
+    private OvertimeHandler model;
+    private ObservableList<OvertimeEntry> entries = FXCollections.observableArrayList();
 
     @Override
     public void update() {
@@ -40,19 +44,53 @@ public class OvertimeWorkHoursController extends Controller{
         if (navBar_container.getChildren().isEmpty()) {
             navBar_container.getChildren().add(Driver.getScreenController().getNavBar());
         }
+
+        Date today = new Date();
+        long ms = today.getTime();
+        model = new OvertimeHandler(new Date(ms - 15 * 86400000L), today);
+        entries.setAll(model.getEntries());
+        overtimeTv.setItems(entries);
     }
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         //setColumnWidth();
         disableReorder();
+
+        overtimeTv.setEditable(true);
+        nameTc.setCellValueFactory(new PropertyValueFactory<>("employeeName"));
+        overtimeTc.setCellValueFactory(new PropertyValueFactory<>("minutes"));
+        dateTc.setCellValueFactory(new PropertyValueFactory<>("dateString"));
+        buttonTc.setCellValueFactory(new PropertyValueFactory<>("status"));
+        buttonTc.setCellFactory(t -> {
+            CheckBox checkBox = new CheckBox();
+            TableCell<OvertimeEntry, Boolean> cell = new TableCell<>() {
+                @Override
+                public void updateItem(Boolean item, boolean empty) {
+                    if (empty) {
+                        setGraphic(null);
+                    } else {
+                        checkBox.setSelected(item);
+                        setGraphic(checkBox);
+                    }
+                }
+            };
+            checkBox.selectedProperty().addListener((obs, oldValue, newValue) -> {
+                if (cell.getTableRow().getItem() != null) {
+                    cell.getTableRow().getItem().setStatus(newValue);
+                }
+            });
+            cell.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            cell.setAlignment(Pos.CENTER);
+            return cell;
+        });
     }
 
     /**
      * This method sets the size of table columns present in OvertimeWorkHours.fxml
      */
 
-    private void setColumnWidth(){
+    private void setColumnWidth() {
         nameTc.prefWidthProperty().bind(overtimeTv.widthProperty().divide(11));
         overtimeTc.prefWidthProperty().bind(overtimeTv.widthProperty().divide(11));
         dateTc.prefWidthProperty().bind(overtimeTv.widthProperty().divide(11));
@@ -69,4 +107,11 @@ public class OvertimeWorkHoursController extends Controller{
         buttonTc.setReorderable(false);
     }
 
+    @FXML
+    private void onCheckAllAction() {
+        for (OvertimeEntry entry: entries) {
+            entry.setStatus(true);
+        }
+        overtimeTv.refresh();
+    }
 }
