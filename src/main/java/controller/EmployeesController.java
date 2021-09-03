4247ac14c02bf8c1d80f84ee325997a55fbf1b58
GianTee20 <giancarlo_tee@dlsu.edu.ph>
2021-09-03 20:38:13 +0800

    modify: small edits to employees tab

--- src/main/java/controller/EmployeesController.java
+++ src/main/java/controller/EmployeesController.java
@@ -2,6 +2,8 @@ package controller;
 
 import driver.Driver;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 public class EmployeesController extends Controller{
@@ -9,11 +11,29 @@ public class EmployeesController extends Controller{
     @FXML
     private AnchorPane navBar_container;
 
+    @FXML
+    private TableView employeesTv;
+
+    @FXML
+    private TableColumn nameTc, frequencyTc, modeTc, companyTc, wageTc, buttonTc;
+
     @Override
     public void update() {
+
+        disableReorder();
+
         // load navigation bar
         if (navBar_container.getChildren().isEmpty()) {
             navBar_container.getChildren().add(Driver.getScreenController().getNavBar());
         }
+    }
+
+    private void disableReorder() {
+        nameTc.setReorderable(false);
+        companyTc.setReorderable(false);
+        frequencyTc.setReorderable(false);
+        buttonTc.setReorderable(false);
+        modeTc.setReorderable(false);
+        wageTc.setReorderable(false);
     }
 }
