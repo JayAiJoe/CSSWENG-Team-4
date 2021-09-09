bbbb820c94d0f1a5483d6e0f2546250b4ad6dbc4
GabPua <gabriel_pua@dlsu.edu.ph>
2021-09-09 22:55:20 +0800

    add: added updateCola method

--- src/main/java/model/EmployeeForm.java
+++ src/main/java/model/EmployeeForm.java
@@ -1,9 +1,11 @@
 package model;
 
+import dao.ColaPOJO;
 import dao.EmployeePOJO;
 import dao.Repository;
 import wrapper.EmployeeWrapper;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -48,5 +50,30 @@ public class EmployeeForm {
 
     public void updateEmployee(EmployeeWrapper employee) {
         Repository.getInstance().updateEmployee(employee.getEmployee());
+    }
+
+    public void updateCola(ArrayList<ColaPOJO> colas, Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+        String today = format.format(date);
+        ArrayList<ColaPOJO> check = Repository.getInstance().getCola(
+                new Date(date.getTime() - 86400000L), new Date(date.getTime() + 86400000L));
+
+        for (ColaPOJO cola: colas) {
+            boolean found = false;
+            for (ColaPOJO checkCola: check) {
+                // check if cola is already in database
+                if (today.equals(format.format(checkCola.getDate())) &&
+                        cola.getEmployeeID() == checkCola.getEmployeeID()) {
+                    found = true;
+                    checkCola.setCola(cola.getCola());
+                    Repository.getInstance().updateCola(checkCola);
+                    break;
+                }
+            }
+            // cola is not yet in database
+            if (!found) {
+                Repository.getInstance().addCola(cola);
+            }
+        }
     }
 }
