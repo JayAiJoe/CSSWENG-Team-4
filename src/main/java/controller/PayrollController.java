d6636f15c65d6e174aa686634beddc66bcf105bc
GianTee20 <giancarlo_tee@dlsu.edu.ph>
2021-09-12 19:44:50 +0800

    modify: added 13th Month column and corresponding toggle function in Payrolls

--- src/main/java/controller/PayrollController.java
+++ src/main/java/controller/PayrollController.java
@@ -47,7 +47,10 @@ public class PayrollController extends Controller {
     private TableColumn<PayrollEntry, String> nameTc, modeTc, absentTc, rateTc, salaryTc,
             amountTc, colaTc, totalTc, sssTc, philhealthTc, pagibigTc, taxTc, lateTc, netTc, wageTc;
     @FXML
-    private TableColumn<PayrollEntry, Integer> workdaysTc, timeTc, overtimeTc, deductionsTc;
+    private TableColumn<PayrollEntry, Integer> workdaysTc, timeTc, overtimeTc, deductionsTc, thirteenTc;
+
+    @FXML
+    private CheckBox toggleThirteen;
 
     private Payroll payroll = null;
     private ObservableList<PayrollEntry> crayolaEntries = FXCollections.observableArrayList(),
@@ -104,25 +107,7 @@ public class PayrollController extends Controller {
         payrollTv.setSelectionModel(null);
 
         // initialize columns
-        initCol(nameTc, "employeeName", 8);
-        initCol(modeTc, "mode", 18);
-        initCol(absentTc, "absent", 20);
-        initCol(workdaysTc, "workdays", 20);
-        initCol(rateTc, "rate", 20);
-        initCol(salaryTc, "salary", 16);
-        initCol(overtimeTc, "overtime", 55);
-        initCol(timeTc, "time", 25);
-        initCol(amountTc, "amount", 20);
-        initCol(colaTc, "cola", 18);
-        initCol(totalTc, "total", 16);
-        initCol(deductionsTc, "deductions", 100);
-        initCol(sssTc, "sss", 20);
-        initCol(philhealthTc, "philhealth", 20);
-        initCol(pagibigTc, "pagibig", 20);
-        initCol(lateTc, "late", 20);
-        initCol(taxTc, "", 20);
-        initCol(netTc, "net", 16);
-        initCol(wageTc, "wage", 16);
+        initPayrollCol();
 
         //disable crayolaBtn upon initialization
         crayolaBtn.setDisable(true);
@@ -176,7 +161,76 @@ public class PayrollController extends Controller {
         }
     }
 
+    /**
+     * Method for returning to Home.fxml
+     */
     public void onHomeClick(){
         Driver.getScreenController().activate("Home");
+    }
+
+
+    /**
+     * Method for adding or removing 13th Month column in the table
+     */
+    public void onThirteenClick(){
+        if(toggleThirteen.isSelected()){
+            initCol(thirteenTc, "13th Month", 16);
+            init13thCol();
+            thirteenTc.setVisible(true);
+        }
+        else{
+            initPayrollCol();
+            thirteenTc.setVisible(false);
+        }
+    }
+
+    /**
+     * Method initializes all columns with 13th Month Pay not being present in the table
+     */
+    private void initPayrollCol(){
+        initCol(nameTc, "employeeName", 8);
+        initCol(modeTc, "mode", 18);
+        initCol(absentTc, "absent", 20);
+        initCol(workdaysTc, "workdays", 20);
+        initCol(rateTc, "rate", 20);
+        initCol(salaryTc, "salary", 16);
+        initCol(overtimeTc, "overtime", 55);
+        initCol(timeTc, "time", 25);
+        initCol(amountTc, "amount", 20);
+        initCol(colaTc, "cola", 18);
+        initCol(totalTc, "total", 16);
+        initCol(deductionsTc, "deductions", 100);
+        initCol(sssTc, "sss", 20);
+        initCol(philhealthTc, "philhealth", 20);
+        initCol(pagibigTc, "pagibig", 20);
+        initCol(lateTc, "late", 20);
+        initCol(taxTc, "", 20);
+        initCol(netTc, "net", 16);
+        initCol(wageTc, "wage", 16);
+    }
+
+    /**
+     * Method Initializes all Columns with 13th Month pay included in the table
+     */
+    private void init13thCol(){
+        initCol(nameTc, "employeeName", 9);
+        initCol(modeTc, "mode", 19);
+        initCol(absentTc, "absent", 21);
+        initCol(workdaysTc, "workdays", 21);
+        initCol(rateTc, "rate", 21);
+        initCol(salaryTc, "salary", 17);
+        initCol(overtimeTc, "overtime", 57);
+        initCol(timeTc, "time", 26);
+        initCol(amountTc, "amount", 21);
+        initCol(colaTc, "cola", 19);
+        initCol(totalTc, "total", 17);
+        initCol(deductionsTc, "deductions", 105);
+        initCol(sssTc, "sss", 21);
+        initCol(philhealthTc, "philhealth", 21);
+        initCol(pagibigTc, "pagibig", 21);
+        initCol(lateTc, "late", 21);
+        initCol(taxTc, "", 21);
+        initCol(netTc, "net", 17);
+        initCol(wageTc, "wage", 17);
     }
 }
