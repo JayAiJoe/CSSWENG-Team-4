338502154ad381396829a49a5daedb7e963bfeef
GabPua <gabriel_pua@dlsu.edu.ph>
2021-09-14 16:51:48 +0800

    fix: fixed small date bug

--- src/main/java/model/AttendanceStatistics.java
+++ src/main/java/model/AttendanceStatistics.java
@@ -5,6 +5,7 @@ import dao.Repository;
 import wrapper.PerformanceWrapper;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AttendanceStatistics {
     ArrayList<PerformanceWrapper> statistics = new ArrayList<>();
@@ -19,6 +20,7 @@ public class AttendanceStatistics {
     private void initialize() {
         ArrayList<PerformancePOJO> performances = Repository.getInstance().getAllPerformance();
         for (PerformancePOJO performance: performances) {
+            performance.setDatePaid(new Date(performance.getDatePaid().getTime() - 8 * 3600000L));
             statistics.add(new PerformanceWrapper(performance));
         }
     }
